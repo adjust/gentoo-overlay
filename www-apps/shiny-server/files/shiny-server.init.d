600cd0bf87dad0d53198817703f9910c1732b90e
Felix Wischke (65278) <felix@adeven.com>
2018-02-12 14:28:03 +0100

    www-apps/shiny-server: npm based package to run shiny apps

---
+++ www-apps/shiny-server/files/shiny-server.init.d
@@ -0,0 +1,21 @@
+#!/sbin/openrc-run
+
+command="/opt/shiny-server/bin/shiny-server"
+pidfile="/var/run/${SVCNAME}.pid"
+
+depend(){
+	use net
+}
+
+start() {
+	ebegin "Starting Shiny Server Daemon"
+	start-stop-daemon --start --quiet --background --exec "$command" \
+		-m --pidfile "$pidfile" --user "shiny" --group "shiny"
+	eend $?
+}
+
+stop() {
+	ebegin "Stopping Shiny daemon"
+	start-stop-daemon --quiet --stop --pidfile $pidfile
+	eend $?
+}
